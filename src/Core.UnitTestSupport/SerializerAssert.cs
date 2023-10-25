60d7e8da9a82e8ac26b16dae8a43bf859b0e411f
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2023-10-25 14:45:44 +0300

    Add UnitTestSupport Library

---
+++ src/Core.UnitTestSupport/SerializerAssert.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Text.Json;
+
+namespace HerzenHelper.Core.UnitTestSupport;
+public static class SerializerAssert
+{
+  public static void AreEqual(object expected, object result)
+  {
+    string expected2 = JsonSerializer.Serialize(expected);
+    string actual = JsonSerializer.Serialize(result);
+    Assert.AreEqual(expected2, actual);
+  }
+
+  public static void AreNotEqual(object expected, object result)
+  {
+    string expected2 = JsonSerializer.Serialize(expected);
+    string actual = JsonSerializer.Serialize(result);
+    Assert.AreNotEqual(expected2, actual);
+  }
+}
